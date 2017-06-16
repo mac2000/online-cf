d44183df66e4cc891dd270b853ef93b55bb6b80d
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-06-16 10:12:34 +0300

    wip

--- web/server.js
+++ web/server.js
@@ -67,4 +67,16 @@ app.get('/user/:user', (req, res) => {
 	client.smembers('u' + user, (err, data) => res.status(err ? 500: 200).json(err ? err : data))
 })
 
+app.get('/item', (req, res) => {
+	client.keys('*', (err, data) => {
+		res.status(err ? 500: 200).json(err ? err : data.filter(i => i.indexOf('i') === 0).map(i => i.substring(1)))
+	})
+})
+
+app.get('/user', (req, res) => {
+	client.keys('*', (err, data) => {
+		res.status(err ? 500: 200).json(err ? err : data.filter(i => i.indexOf('u') === 0).map(i => i.substring(1)))
+	})
+})
+
 app.listen(process.env.PORT || 3000)
